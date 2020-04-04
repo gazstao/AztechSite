4df732e5a14a4f024846d23f9b9c12fa0af10d47
Gazstao <gazstao@gmail.com>
2020-04-04 10:42:24 -0300

    drum kit added

--- Gazstao/dicee/dicee.js
+++ Gazstao/dicee/dicee.js
@@ -1,6 +1,7 @@
 // alert("May the lucky be with you!");
 var randomNumber1 = Math.floor(Math.random()*6+1);
 var randomNumber2 = Math.floor(Math.random()*6+1);
+
 var img1 = "";
 var img2 = "";
 
@@ -10,6 +11,6 @@ img2 = "dice" + randomNumber2 + ".png";
 document.querySelector(".img1").attributes.src.value = "images/"+img1;
 document.querySelector(".img2").attributes.src.value = "images/"+img2;
 
-if ( randomNumber1 > randomNumber2 ) document.querySelector("h1").innerHTML = "Player 1 Wins!" ;
-else if ( randomNumber2 > randomNumber1 ) document.querySelector("h1").innerHTML = "Player 2 Wins!" ;
-else document.querySelector("h1").innerHTML = "We have a Draw!" ;
+if ( randomNumber1 > randomNumber2 ) document.querySelector("h1").innerHTML = "Jogador 1 Venceu!" ;
+else if ( randomNumber2 > randomNumber1 ) document.querySelector("h1").innerHTML = "Jogador 2 Venceu!" ;
+else document.querySelector("h1").innerHTML = "Temos um empate!" ;
