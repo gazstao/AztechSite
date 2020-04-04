9f8e7aa93dd9a47ea72c3595795e259823c9c88f
Gazstao <gazstao@gmail.com>
2020-04-04 19:52:26 -0300

    updated

--- Gazstao/drummer/index.js
+++ Gazstao/drummer/index.js
@@ -1,7 +1,15 @@
 var numeroDeTeclas = document.querySelectorAll(".drum").length;
 
+var crash = new Audio("sounds/crash.mp3");
+var kick = new Audio ("sounds/kick-bass.mp3");
+var snare = new Audio("sounds/snare.mp3");
+var tom1 = new Audio ("sounds/tom-1.mp3");
+var tom2 = new Audio ("sounds/tom-2.mp3");
+var tom3 = new Audio ("sounds/tom-3.mp3");
+var tom4 = new Audio ("sounds/tom-4.mp3");
+
 for (var i = 0 ; numeroDeTeclas ; i++ ){
     document.querySelectorAll(".drum")[i].addEventListener("click", function (){
-        alert("Fui clicado!");
+        crash.play();
     });
 }
