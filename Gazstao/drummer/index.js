bbfb82e0be8b472b817beebc6f5e8f0c33dfd848
Gazstao <gazstao@gmail.com>
2020-04-04 21:23:49 -0300

    sounds working on click

--- Gazstao/drummer/index.js
+++ Gazstao/drummer/index.js
@@ -8,8 +8,53 @@ var tom2 = new Audio ("sounds/tom-2.mp3");
 var tom3 = new Audio ("sounds/tom-3.mp3");
 var tom4 = new Audio ("sounds/tom-4.mp3");
 
-for (var i = 0 ; numeroDeTeclas ; i++ ){
+for (var i=0 ; i < numeroDeTeclas ; i++ ) {
+
     document.querySelectorAll(".drum")[i].addEventListener("click", function (){
-        crash.play();
+
+      var keyPressed = this.innerHTML;
+
+        switch (keyPressed) {
+
+          case "a":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+
+          case "s":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+
+          case "d":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+
+          case "f":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+
+          case "j":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+
+          case "k":
+            var kick = new Audio("sounds/kick-bass.mp3");
+            kick.play();
+            break;
+
+          case "l":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+
+          default:
+            console.log(keyPressed);
+        }
+
     });
+
 }
