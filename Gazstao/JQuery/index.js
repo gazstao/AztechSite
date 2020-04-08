99b8ed7fbe46195ef57de32bafb749f295ffefbf
gazstao <gazstao@gmail.com>
2020-04-08 19:59:56 -0300

    updated class 185

--- Gazstao/JQuery/index.js
+++ Gazstao/JQuery/index.js
@@ -1,2 +1,14 @@
 //alert("JavaScript is Working Fine!");
 $("h1").css("background-color","black");
+$("h1").css("padding","1rem");
+//$("h1").css("max-width","40%");
+
+$("button").addClass("spaced-little rounded-button");
+$("a").addClass("spaced");
+
+$("h1").text("Gazstao Funny Site");
+$("button").html("<em>Click</em>");
+
+console.log("Cor: "+$("h1").css("color"));
+console.log("Font Size: "+$("h1").css("font-size"));
+console.log("Botao arredondado:"+$("button").hasClass("rounded-button"));
