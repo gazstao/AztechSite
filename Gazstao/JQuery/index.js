abd07c68e5e99440aaab2e46b9c92e72b4325760
gazstao <gazstao@gmail.com>
2020-04-09 19:40:52 -0300

    javascript new features

--- Gazstao/JQuery/index.js
+++ Gazstao/JQuery/index.js
@@ -1,22 +1,40 @@
-//alert("JavaScript is Working Fine!");
+
+// Alterando Atributos CSS
+
 $("h1").css("background-color","black");
 $("h1").css("padding","1rem");
-//$("h1").css("max-width","40%");
+
+// Adicionando Classes
 
 $("button").addClass("spaced-little rounded-button");
 $("a").addClass("spaced");
 
+// Alterando o texto, html e atributos
+
 $("h1").text("Gazstao Funny Site");
 $("button").html("<em>Click</em>");
 $("img").attr("src", "cloud.png");
+
+// Console Log de vários parâmetros
 
 console.log("Cor: "+                $("h1").css("color")                    );
 console.log("Font Size: "+          $("h1").css("font-size")                );
 console.log("Botao arredondado:"+   $("button").hasClass("rounded-button")  );
 console.log("Imagem:"+              $("img").attr("src")                    );
 
+// Adicionando Listener
+
+$("img").click(mudaFigura);
+$("h1").click(mudaClasse);
+
+// Funções
+
 function mudaFigura(){
   if ($("img").attr("src") == "mountain.png") $("img").attr("src","cloud.png");
   else $("img").attr("src","mountain.png");
   console.log("Imagem alterada para "+    $("img").attr("src"));
+}
+
+function mudaClasse(){
+  $("h1").toggleClass("inverted");
 }
