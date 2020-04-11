b49e035f47c9ff66a70fc601aabf8428b8b801ed
Gazstao <gazstao@MacBook-de-Gazstao.local>
2020-04-11 10:31:52 -0300

    Solution 9 added

--- Gazstao/Genius/Simon Game Challenge Step 9 Answer/game.js
+++ Gazstao/Genius/Simon Game Challenge Step 9 Answer/game.js
@@ -4,17 +4,11 @@ var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
 
-//You'll need a way to keep track of whether if the game has started or not, so you only call nextSequence() on the first keypress.
 var started = false;
-
-//2. Create a new variable called level and start at level 0.
 var level = 0;
 
-//1. Use jQuery to detect when a keyboard key has been pressed, when that happens for the first time, call nextSequence().
 $(document).keypress(function() {
   if (!started) {
-
-    //3. The h1 title starts out saying "Press A Key to Start", when the game has started, change this to say "Level 0".
     $("#level-title").text("Level " + level);
     nextSequence();
     started = true;
@@ -28,14 +22,46 @@ $(".btn").click(function() {
 
   playSound(userChosenColour);
   animatePress(userChosenColour);
+
+  checkAnswer(userClickedPattern.length-1);
 });
+
+
+function checkAnswer(currentLevel) {
+
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+
+      console.log("success");
+
+      if (userClickedPattern.length === gamePattern.length){
+        setTimeout(function () {
+          nextSequence();
+        }, 1000);
+      }
+
+    } else {
+
+      console.log("wrong");
+
+      //1. In the sounds folder, there is a sound called wrong.mp3, play this sound if the user got one of the answers wrong.
+      playSound("wrong");
+
+      //2. In the styles.css file, there is a class called "game-over", apply this class to the body of the website when the user gets one of the answers wrong and then remove it after 200 milliseconds.
+      $("body").addClass("game-over");
+      setTimeout(function () {
+        $("body").removeClass("game-over");
+      }, 200);
+
+      //3. Change the h1 title to say "Game Over, Press Any Key to Restart" if the user got the answer wrong.
+      $("#level-title").text("Game Over, Press Any Key to Restart");
+    }
+
+}
 
 function nextSequence() {
 
-  //4. Inside nextSequence(), increase the level by 1 every time nextSequence() is called.
+  userClickedPattern = [];
   level++;
-
-  //5. Inside nextSequence(), update the h1 with this change in the value of level.
   $("#level-title").text("Level " + level);
 
   var randomNumber = Math.floor(Math.random() * 4);
