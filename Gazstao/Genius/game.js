cac4354a8c37f6328695a88de5267b0063a4bfc0
Gazstao <gazstao@gmail.com>
2020-04-10 23:01:48 -0300

    capturando teclas e cliques, tocando som e piscando o botao

--- Gazstao/Genius/game.js
+++ Gazstao/Genius/game.js
@@ -7,3 +7,37 @@ function nextSequence(){
   gamePattern.push(randomChosenColour);
   return randomChosenColour;
 }
+
+function piscaBotao(cor){
+  var som = new Audio("sounds/"+cor+".mp3");
+  som.play();
+  $("."+cor).animate({opacity:0}, 80);
+  $("."+cor).animate({opacity:1}, 80);
+}
+
+function clica(cor){
+  piscaBotao(cor);
+}
+
+$(".green").click( function (){
+  clica("green");
+});
+
+$(".red").click( function (){
+  clica("red");
+});
+
+$(".yellow").click( function (){
+  clica("yellow");
+});
+
+$(".blue").click( function (){
+  clica("blue");
+});
+
+$(document).keydown(function (event){
+  if (event.key.toLowerCase() == "a") clica("green");
+  if (event.key.toLowerCase() == "x") clica("blue");
+  if (event.key.toLowerCase() == "z") clica("yellow");
+  if (event.key.toLowerCase() == "s") clica("red");
+});
