afd25706d6a29b1ebe382e4e1b5b164050c48cab
Gazstao <gazstao@gmail.com>
2020-04-11 00:09:17 -0300

    Updated class 207 self solution

--- Gazstao/Genius/game.js
+++ Gazstao/Genius/game.js
@@ -1,45 +1,58 @@
-var buttonColors = [ "red" , "blue" , "green" , "yellow"];
+var buttonColors = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userPattern = [];
+var level = 0;
+gameStarted = false;
 
-function nextSequence(){
-  var num = Math.floor(Math.random()*4);
+function nextSequence() {
+  var num = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColors[num];
   gamePattern.push(randomChosenColour);
-  return randomChosenColour;
+  $("h1").text("Level "+level);
+  level++;
+  piscaBotao(randomChosenColour);
 }
 
-function piscaBotao(cor){
-  var som = new Audio("sounds/"+cor+".mp3");
+function piscaBotao(cor) {
+  var som = new Audio("sounds/" + cor + ".mp3");
   som.play();
-  $("."+cor).animate({opacity:0}, 80);
-  $("."+cor).animate({opacity:1}, 80);
+  $("." + cor).animate({
+    opacity: 0
+  }, 80);
+  $("." + cor).animate({
+    opacity: 1
+  }, 80);
 }
 
-function clica(cor){
-  piscaBotao(cor);
-  userPattern.push(cor);
+function clica(cor) {
+  if (gameStarted) {
+    piscaBotao(cor);
+    userPattern.push(cor);
+  }
 }
 
-$(".green").click( function (){
+// Adiciona listeners para os cliques
+$(".green").click(function() {
   clica("green");
 });
-
-$(".red").click( function (){
+$(".red").click(function() {
   clica("red");
 });
-
-$(".yellow").click( function (){
+$(".yellow").click(function() {
   clica("yellow");
 });
-
-$(".blue").click( function (){
+$(".blue").click(function() {
   clica("blue");
 });
 
-$(document).keydown(function (event){
+// Adiciona listeners para as teclas
+$(document).keydown(function(event) {
   if (event.key.toLowerCase() == "a") clica("green");
-  if (event.key.toLowerCase() == "x") clica("blue");
-  if (event.key.toLowerCase() == "z") clica("yellow");
   if (event.key.toLowerCase() == "s") clica("red");
+  if (event.key.toLowerCase() == "z") clica("yellow");
+  if (event.key.toLowerCase() == "x") clica("blue");
+  if (!gameStarted){
+    gameStarted = true;
+    nextSequence();
+  }
 });
