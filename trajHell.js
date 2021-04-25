cef3eee02dee19724c3dae22fe54ecc072729d9e
gazstao <gazstao@gmail.com>
2021-04-25 13:41:36 -0300

    updated

---
+++ trajHell.js
@@ -0,0 +1,5 @@
+$(".trajVideo").click(function(){ window.open("trajanoVideo.html","blank");});
+$(".materia").click(function(){window.open("https://www.plural.jor.br/noticias/cultura/a-trajano-reis-virou-jogo/","blank");});
+$(".download").click(function(){window.open("https://drive.google.com/file/d/1THdvv84LJh5slFUsXkKTrI8J6GH5xtTa/view?usp=sharing", "blank");});
+$(".oficial").click(function(){window.open("https://valentino5cordas.wixsite.com/valentino/game-trajano-hell", "blank");});
+$(".contato").click(function(){window.open("https://valentino5cordas.wixsite.com/valentino/game-trajano-hell", "blank");});
